eb4c1f178423ca381ab8f91bc8b587fe5862a30c
Özge Poyraz <71945096+ozgepoyraz@users.noreply.github.com>
2022-07-04 13:47:35 +0300

    initial commit

--- BirthdayNoteTaker/ViewController.swift
+++ BirthdayNoteTaker/ViewController.swift
@@ -9,11 +9,45 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var birthdayLabel: UILabel!
+    @IBOutlet weak var name: UITextField!
+    @IBOutlet weak var birthday: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let storedName = UserDefaults.standard.object(forKey: "name")
+        let storedBirthday = UserDefaults.standard.object(forKey: "birthday")
+        
+        if let newName = storedName as? String{
+            nameLabel.text = "Name: \(newName)"
+        }
+        if let newBirthday = storedBirthday as? String{
+            birthdayLabel.text = "Birthday: \(newBirthday)"
+        }
     }
-
-
+    @IBAction func saveButton(_ sender: Any) {
+        UserDefaults.standard.set(name.text!, forKey: "name")
+        UserDefaults.standard.set(birthday.text!, forKey: "birthday")
+        
+        nameLabel.text = "Name: \(name.text!)"
+        birthdayLabel.text = "Birthday: \(birthday.text!)"
+    }
+    
+    @IBAction func deleteButton(_ sender: Any) {
+        let storedName = UserDefaults.standard.object(forKey: "name")
+        let storedBirthday = UserDefaults.standard.object(forKey: "birthday")
+        
+        if (storedName as? String) != nil {
+            UserDefaults.standard.removeObject(forKey: "name")
+            name.text = " "
+            nameLabel.text = "Name: "
+        }
+        if (storedBirthday as? String) != nil{
+            UserDefaults.standard.removeObject(forKey: "birthday")
+            birthday.text = " "
+            birthdayLabel.text  = "Birthday: "
+        }
+    }
 }
 
